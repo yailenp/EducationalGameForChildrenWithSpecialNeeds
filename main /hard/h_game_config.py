4e3fbe835a926135b4f5b1a6aa091d127eb76cef
yailenp <86435645+yailenp@users.noreply.github.com>
2021-07-21 19:26:57 -0400

    Add files via upload

---
+++ main /hard/h_game_config.py
@@ -0,0 +1,21 @@
+import os
+
+IMAGE_SIZE = 170
+SCREEN_WIDTH = 1350
+SCREEN_HEIGHT = 730
+
+#Matrix size for hard level
+NUM_TILES_SIDE = 6
+NUM_TILES_TOTAL = 24
+MARGIN = 10
+
+#CENTERING MATRIX
+NUM_ROWS = 4
+NUM_COLM = 6
+
+T_B_MARG = (SCREEN_HEIGHT - (IMAGE_SIZE * NUM_ROWS) + (MARGIN *2)) // 2
+R_L_MARG = (SCREEN_WIDTH - (IMAGE_SIZE * NUM_COLM) + (MARGIN *2))  // 2
+
+ASSET_DIR = 'hard/hard_assets'
+ASSET_FILES = [x for x in os.listdir(ASSET_DIR) if x[-3:].lower() == 'png']
+assert len(ASSET_FILES) == 12
